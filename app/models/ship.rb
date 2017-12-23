dc5e1c9d97078bb57efa2b18bcfb8cf904b875cd
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-23 22:23:38 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,10 +3,10 @@ class Ship
   attr_accessor :name, :type, :booty
   @@all = []
 
-  def initialize(args)
-    @name = args[:name]
-    @type = args[:type]
-    @booty = args[:booty]
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
 
     @@all << self
   end
